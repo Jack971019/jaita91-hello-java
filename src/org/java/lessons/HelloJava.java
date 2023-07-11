dd5ef8a9c52a407b55e4cfe76ef05c182e2d26b5
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-11 12:34:13 +0200

    project set up e classe helloJava

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,8 @@
+package org.java.lessons;
+
+public class HelloJava {
+    public static void main(String[] args) {
+      System.out.println("this is my first java program");
+        System.out.println("bye bye");
+    }
+}
